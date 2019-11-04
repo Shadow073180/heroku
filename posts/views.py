69f601145b50fc962ad0235fbe9a0020bdc0f11b
shadow073180 <david73180@gmail.com>
2019-11-04 17:31:15 -0500

    makin it better

---
+++ posts/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics,serializers
+from django.db import models
+from IPython import embed
+
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
